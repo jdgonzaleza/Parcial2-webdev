14881de520759ecb426083cb52a5d04b24a7ce34
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-05-11 20:24:19 -0500

    el settings

--- imports/ui/MapContainer.jsx
+++ imports/ui/MapContainer.jsx
@@ -16,7 +16,8 @@ export class MapContainer extends React.Component {
         <small>{this.props.elMensaje}</small>
         <Map google={this.props.google} markers={this.props.markers} />
 
-
+        <script async defer src={"https://maps.googleapis.com/maps/api/js?key=" + Meteor.settings.GOOGLE_MAPS_API + "&callback=initMap"}
+          type="text/javascript"></script>
       </div>
     );
   }
