14dd741922a506d776e7b7ee868c206eafc4a6db
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-05-11 20:12:15 -0500

    el settings

--- imports/ui/MapContainer.jsx
+++ imports/ui/MapContainer.jsx
@@ -15,7 +15,7 @@ export class MapContainer extends React.Component {
         <h1>Map with the Bus Stops</h1>
         <small>{this.props.elMensaje}</small>
         <Map google={this.props.google} markers={this.props.markers} />
-        <script src={"https://maps.google.com/maps/api/js?key=" + Meteor.setting.GOOGLE_MAPS_API} type="text/javascript"></script>
+        <script src={"https://maps.google.com/maps/api/js?key=" + Meteor.settings.GOOGLE_MAPS_API} type="text/javascript"></script>
 
       </div>
     );
